a2b81b790475535c2732d99a7a78256f810b6eb9
Cade Larrabee <larrabeecade@gmail.com>
2024-05-06 10:23:28 -0400

    node module start

--- src/nodeModule.js
+++ src/nodeModule.js
@@ -1,19 +1,65 @@
 import { parse } from "date-fns";
 export class Node {
-  constructor() {
-    //this.id = id;
+  //
+  //A node is a basic data structure that knows two things.
+  //What its connected to, behind it, and ahead of it. "next" and "previous"
+  //what data it stores,
+  //Whether it is a head node, or a tail node.
+  //Tail nodes have no next, and head nodes have no previous.
+  //
+  constructor(isHead, isTail, Data, nextNode, prevNode) {
+    isHead = this.isHead;
+    isTail = this.isTail;
+    Data = this.data;
+    nextNode = this.nextNode;
+    prevNode = this.prevNode;
   }
-  displayNodeDetails() {}
+  displayNodeDetails() {
+    console.log(this.Data, this.prevNode, this.nextNode);
+  }
   //
-  //Getters
+  // Getters
   //
-  // get taskName() {
-  //   return this.name;
-  // }
+  getIsHead() {
+    return this.isHead;
+  }
+
+  getIsTail() {
+    return this.isTail;
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  getNextNode() {
+    return this.nextNode;
+  }
+
+  getPrevNode() {
+    return this.prevNode;
+  }
+
   //
-  //Setters
+  // Setters
   //
-  // set taskName(newName) {
-  //   this.name = newName;
-  // }
+  setIsHead(isHead) {
+    this.isHead = isHead;
+  }
+
+  setIsTail(isTail) {
+    this.isTail = isTail;
+  }
+
+  setData(data) {
+    this.data = data;
+  }
+
+  setNextNode(nextNode) {
+    this.nextNode = nextNode;
+  }
+
+  setPrevNode(prevNode) {
+    this.prevNode = prevNode;
+  }
 }
